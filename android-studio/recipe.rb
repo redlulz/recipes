5460f36429b7ce830fea393016af819fc15835f6
Ole Harth <oleharth@gmail.com>
2016-11-25 17:57:13 +0100

    add Android Studio recipe

---
+++ android-studio/recipe.rb
@@ -0,0 +1,40 @@
+class Studio < FPM::Cookery::Recipe
+  description   'The Official IDE for Android'
+
+  name          'android-studio'
+  version       '2.2.2.0'
+  revision      2
+  buildid =     '145.3360264'
+  section       'devel'
+  vendor        'Google'
+  homepage      'https://developer.android.com/studio/index.html'
+  source        "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-#{buildid}-linux.zip"
+  sha1          'fc63ca247762697c33102a78063a95f8b5ab5dea'
+
+  depends       'default-jdk'
+
+  config_files  %W(
+    /opt/#{name}/bin/idea.properties
+    /opt/#{name}/bin/studio.vmoptions
+    /opt/#{name}/bin/studio.vmoptions
+  )
+
+  def build
+  end
+
+  def install
+    opt("#{name}").install Dir['*']
+    opt("#{name}/jre").rmtree
+
+    share('applications').install workdir('android-studio.desktop')
+
+    share('pixmaps').mkpath
+    bin.mkpath
+    iconlink = share('pixmaps/android-studio.png')
+    executablelink = bin('android-studio')
+    with_trueprefix do
+      iconlink.make_symlink opt("#{name}/bin/studio.png")
+      executablelink.make_symlink opt("#{name}/bin/studio.sh")
+    end
+  end
+end
