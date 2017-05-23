6cfad59726bb832dd90b8d41f13ddcd7044c14db
Unknown <oleharth@gmail.com>
2017-05-23 15:30:56 +0200

    update Rider to EAP 22

--- rider/recipe.rb
+++ rider/recipe.rb
@@ -2,14 +2,13 @@ class Rider < FPM::Cookery::Recipe
   description   'A cross-platform .NET IDE based on the IntelliJ platform and ReSharper'
 
   name          'rider'
-  buildid =     '171.4089.466'
-  version       "1.0-EAP-21-#{buildid}"
+  buildid =     '171.4456.199'
+  version       "1.0-EAP-22-#{buildid}"
   revision      1
   section       'devel'
   vendor        'JetBrains'
   homepage      'https://www.jetbrains.com/rider/'
   source        "https://download.jetbrains.com/resharper/riderRS-#{buildid}.tar.gz"
-  sha256        'cccebfa1d0b35491099f4f49fc005102e9aad10b5f56cfd9c2551615b00dda6e'
 
   depends       'default-jre', 'mono-devel'
 
@@ -24,7 +23,7 @@ class Rider < FPM::Cookery::Recipe
 
   def install
     opt("#{name}").install Dir['*']
-    opt("#{name}/jre").rmtree
+    opt("#{name}/jre64").rmtree
 
     share('applications').install workdir('rider.desktop')
 
